0d048c79435e1088bd0ca7371b5f04348b124580
stablegenius10 <115222029+stablegenius10@users.noreply.github.com>
2024-08-01 13:47:06 +0530

    Update db.js
    old commits

--- db.js
+++ db.js
@@ -1,16 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const dbConnect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error("Could not connect to MongoDB", err);
-  }
-};
-
-module.exports = dbConnect;
+mongoose
+  .connect("mongodb+srv://ayush:ayush@cluster0.hkkuuyw.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
